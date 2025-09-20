31face1c2ba5375ba980cc3f0604db85f0b7a1f4
kurama <paul.zenker.pz@gmail.com>
2025-09-20 15:31:14 +0200

    api: extended PartialAnime

--- src/models/anime.py
+++ src/models/anime.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, field
+from operator import ge
 from typing import List, Any, Dict, Optional, Union
-from datetime import datetime
+from datetime import datetime, timedelta
 
 @dataclass
 class Themes:
     openings: Optional[List[str]]
     endings: Optional[List[str]]
 
+@dataclass
 class Airing: 
     start: Optional[datetime]
     stop: Optional[datetime]
@@ -16,13 +18,52 @@ class Title:
     main: str
     english: str
     japanese: str 
+    alternatives: List[str] = field(default_factory=List[str])
+
+@dataclass
+class Genre:
+    id: int
+    name: str
+
+class Statistic:
+    ...
+
+class Recommendation:
+    ... # maybe sames as PartialAnime?
+
+@dataclass
+class MainPictures:
+    medium: str
+    large: str
+
+@dataclass
+class Studio:
+    id: int
+    name: str
 
 @dataclass
 class PartialAnime:
     id: int
     title: Title
+    airing: Airing
+    synopsis: str
+    mean: float
+    rank: Optional[int]
+    popularity: Optional[int]
     score: Optional[float]
     episodes: Optional[int]
+    nsfw: str
+    created_at: datetime
+    updated_at: Optional[datetime]
+    media_type: str
+    status: str
+    genres: List[Genre]
+    source: Optional[str]
+    average_episode_duration: Optional[timedelta]
+    rating: Optional[str]
+    studios: List[Studio]
+    main_pictures: MainPictures
+
 
 
 
@@ -36,28 +77,12 @@ class Anime(PartialAnime):
     popularity: Optional[int]
     related: Dict[str, List[Dict[str, Any]]]
     themes: Themes
-    rating: str
     rank: Optional[int]
     source: Optional[str]
     status: str
     airing: Airing
     image_url: str
-    studios: List[Dict[str, str]]
-
-    genres: List[Dict[str, str]]
     statistics: Dict[str, Union[Dict[str, str], str, int]]
     recommendations: Optional[List[Dict[str, Dict[str, str]]]]
     title_synopsis: List[str] = field(default_factory=List[str])
 
-
-class Genre:
-    ...
-
-class Statistic:
-    ...
-
-class Recommendation:
-    ... # maybe sames as PartialAnime?
-
-class Studio:
-    ...
