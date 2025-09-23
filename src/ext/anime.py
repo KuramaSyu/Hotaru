e8072c74d3d7f50fea0d3608000e444719d4dc1a
kurama <paul.zenker.pz@gmail.com>
2025-09-23 13:49:54 +0200

    feat: implemented working anime command

--- src/ext/anime.py
+++ src/ext/anime.py
@@ -1,5 +1,9 @@
 from lightbulb import Loader, invoke
 import lightbulb
+import miru
+
+from services.impl.my_anime_list import MyAnimeListRestService
+from views import AnimeNavigationBuilder
 
 loader = Loader()
 
@@ -9,6 +13,18 @@ class Anime(
     name="anime",
     description="Get information about an anime",
 ):
+    search = lightbulb.string("search", "What anime?")
+
     @invoke
-    async def invoke(self, ctx: lightbulb.Context) -> None:
-        await ctx.respond("This is a placeholder for the anime command.")
+    async def invoke(
+        self, 
+        ctx: lightbulb.Context, 
+        anime_service: MyAnimeListRestService, 
+        client: miru.Client
+    ) -> None:
+            nav_getter = AnimeNavigationBuilder(self.search, anime_service)
+            navigator = await nav_getter.get_navigator()
+
+            builder = await navigator.build_response_async(client)
+            await builder.create_initial_response(ctx.interaction)
+            client.start_view(navigator)
