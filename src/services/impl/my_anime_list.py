e340a4bcd93021fce30731bc35899ccfcdd6a2b6
kurama <paul.zenker.pz@gmail.com>
2025-09-23 09:24:18 +0200

    refacor: adopt to MAL API

--- src/services/impl/my_anime_list.py
+++ src/services/impl/my_anime_list.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import *
+from typing import Sequence, List, Optional, Dict
 from urllib.parse import urlencode
 from datetime import datetime
 from pprint import pformat as pf
@@ -11,15 +11,25 @@ import traceback
 
 import aiohttp
 import asyncio
-from attr import dataclass
+from dataclasses import dataclass
+from dataclasses_json import DataClassJsonMixin
 from fuzzywuzzy import fuzz # type: ignore
 from expiring_dict import ExpiringDict # type: ignore
 from pprint import pformat
 
-# from models.anime import PartialAnime
-
+from models.anime import PartialAnime, Anime
+from services.api import AnimeService
 
 log = logging.getLogger(__name__)
+
+
+@dataclass
+class SearchEntry(DataClassJsonMixin):
+    node: PartialAnime
+
+@dataclass
+class MyAnimeListSearchRoot(DataClassJsonMixin):
+    data: List[SearchEntry]
 
 class MALRatings(Enum):
     g = "G - All Ages"
@@ -36,7 +46,7 @@ class MALTypes(Enum):
     ANIME = 1
     MANGA = 2
 
-class MyAnimeListRestService:
+class MyAnimeListRestService(AnimeService):
     """Wrapper for MyAnimeList API Endpoint"""
     client_id: str = ""
     TTL = 60*60
@@ -71,7 +81,7 @@ class MyAnimeListRestService:
         endpoint: str,
         value: Optional[str] = None,
         optional_query: Dict[str, str] | None = None,
-    ) -> Dict[str, Any]:
+    ) -> str:
         query = None
         if value and not value.startswith("/"):
             value = "/" + value
@@ -79,7 +89,7 @@ class MyAnimeListRestService:
             query = f"?{urlencode(optional_query)}"
         url = f"{self._base_url}/{endpoint}{value or ''}{query or ''}"
         async with self.session.get(url, headers=self.headers) as resp:
-            json = await resp.json(encoding="utf-8")
+            json = await resp.text()
         if self._session:
             await self._session.close()
         self.log.debug(f"request: {url}")
@@ -97,7 +107,7 @@ class MyAnimeListRestService:
     async def fetch_anime(
         self,
         id: int
-    ) -> Dict[str, Any]:
+    ) -> Anime:
         """fetch an Anime by it's ID
         
         Args:
@@ -120,7 +130,7 @@ class MyAnimeListRestService:
             value=str(id),
             optional_query={"fields": fields}
         )
-        return resp
+        return Anime.from_json(resp) 
 
     async def _search(self) -> None:
         pass
@@ -130,7 +140,7 @@ class MyAnimeListRestService:
         query: str, 
         include_nsfw: bool = True, 
         fallback: bool = False
-    ) -> Dict[str, Any]:
+    ) -> Sequence[PartialAnime]:
         """search for anime by name
 
         Args:
@@ -172,32 +182,16 @@ class MyAnimeListRestService:
                     "limit":"50", 
                     **kwargs
             })
+            parsed = MyAnimeListSearchRoot.from_json(resp)
+
         except RuntimeError as e:
             if fallback:
                 log.warning(f"Error while fetching anime - title len = {len(query)}")
                 log.warning(traceback.format_exc())
-                return {}
+                return []
             else:
-                log.warning(f"fallback search for title {query}")
+                log.warning(f"search again with nsfw for title {query}")
                 return await self.search_anime(query[:50], include_nsfw, True)
-        log.info(f"fetched {len(resp['data'])} anime in {(datetime.now() - a).total_seconds():.2f}s")
+        log.info(f"fetched {len(parsed.data)} anime in {(datetime.now() - a).total_seconds():.2f}s")
         self.response_cache.ttl(query, deepcopy(resp), self.TTL)
-        return deepcopy(resp)
-
-# @dataclass
-# class SearchEntry:
-#     node: PartialAnime
-
-# @dataclass
-# class MyAnimeListSearchRoot:
-#     data: List[SearchEntry]
-
-if __name__ == "__main__":
-    import os
-    import json
-    mal = MyAnimeListRestService("3203ce3277af7e71ca5eabb7e8298c7b")
-    async def main() -> None:
-        r = await mal.fetch_anime(34599)
-        json.dump(r, open("mal_fetch_example.json", "w"), indent=4)
-        
-    asyncio.run(main())
+        return [entry.node for entry in parsed.data]
